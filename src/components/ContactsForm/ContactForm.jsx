3c43129fc535fbed4e5964f0fd81c1bb6df7569d
jokerav <50701756+jokerav@users.noreply.github.com>
2022-05-29 23:52:40 +0300

    Update ContactForm.jsx

--- src/components/ContactsForm/ContactForm.jsx
+++ src/components/ContactsForm/ContactForm.jsx
@@ -3,15 +3,17 @@ import { Component } from 'react';
 class ContactForm extends Component {
   state = {
     name: '',
+    number: '',
   };
   onChange = e => {
-    this.setState({ name: e.currentTarget.value });
+    const { name, value } = e.currentTarget;
+    this.setState({ [name]: value });
   };
   onSubmit = e => {
     e.preventDefault();
-
     this.props.onSubmit(this.state);
     this.setState({ name: '' });
+    this.setState({ number: '' });
   };
 
   render() {
@@ -26,6 +28,18 @@ class ContactForm extends Component {
             name="name"
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+        <label>
+          Number
+          <input
+            value={this.state.number}
+            onChange={this.onChange}
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
           />
         </label>
