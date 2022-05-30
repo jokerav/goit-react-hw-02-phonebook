b48affdc4bd28e3b55bf73590bf6ce5246bee2f0
jokerav <50701756+jokerav@users.noreply.github.com>
2022-05-30 19:03:26 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,10 +4,12 @@ import ContactForm from './ContactsForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 export class App extends Component {
   static defaultProps = {
-    contacts: [    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
   };
   state = {
     contacts: this.props.contacts,
@@ -24,9 +26,9 @@ export class App extends Component {
         style={{
           // height: '100vh',
           // display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
+          // justifyContent: 'center',
+          // alignItems: 'center',
+          fontSize: 24,
           color: '#010101',
         }}
       >
